fc852cd51c83e54b8b790470afecfed1a837c125
Ghousiah <31919253+Ghousiah@users.noreply.github.com>
2017-09-14 18:18:05 +0530

    MyHome
    It is the basic code in python, where I am going to draw an house

---
+++ MyHome.py
@@ -0,0 +1,51 @@
+import turtle
+myTurtle=turtle.Turtle()
+
+for i in range(4):
+    myTurtle.left(90)
+    myTurtle.forward(100)
+
+for i in range(3):
+    myTurtle.forward(200)
+    myTurtle.left(90)
+    
+myTurtle.forward(100)
+myTurtle.left(90)
+myTurtle.forward(300)
+
+for i in range(2):
+    myTurtle.left(120)
+    myTurtle.forward(100)
+
+myTurtle.left(30)
+myTurtle.forward(100)
+
+for i in range(2):    
+    myTurtle.left(90)
+    myTurtle.forward(100)
+    
+myTurtle.right(90)
+myTurtle.backward(400)
+myTurtle.left(60)
+myTurtle.forward(100)
+myTurtle.right(120)
+myTurtle.forward(100)
+myTurtle.left(150)
+myTurtle.forward(100)
+myTurtle.right(50)
+myTurtle.forward(120)
+myTurtle.right(75)
+myTurtle.forward(132)
+myTurtle.right(55)
+myTurtle.forward(200)
+myTurtle.right(90)
+myTurtle.forward(75)
+myTurtle.right(90)
+myTurtle.forward(50)
+
+for i in range(2):
+    myTurtle.left(90)
+    myTurtle.forward(50)
+    
+myTurtle.right(90)
+myTurtle.forward(75)
